9b2601943ff50f0d0f99aae08ec61a7f9e5b5abb
victor <victor.huecas@gmail.com>
2025-06-10 22:52:23 +0200

    Primer commit: proyecto Spring Boot Fondos viajeros

---
+++ src/main/java/com/vhs/fondosviajeros/FondosviajerosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vhs.fondosviajeros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FondosviajerosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FondosviajerosBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vhs/fondosviajeros/FondosviajerosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vhs.fondosviajeros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FondosviajerosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
